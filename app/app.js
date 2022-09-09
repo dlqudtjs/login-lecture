bddc0cbab34bcbbe8e1e0187d9b179477caed872
dlqudtjs <dlqudtjs730@naver.com>
2022-09-09 20:22:53 +0900

    프런트 간 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views"); //화면 뷰를 저장될 곳
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드
 
