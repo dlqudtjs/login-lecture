460ba77083ca1292d17ac8fd775fd78abaea6975
dlqudtjs <dlqudtjs730@naver.com>
2022-09-09 20:11:58 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱 세팅
-app.set("views", "./views"); //화면 뷰를 저장될 곳
+app.set("views", "./src/views"); //화면 뷰를 저장될 곳
 app.set("view engine", "ejs");
 
 app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드
