1b415bd684ad844b02a3e30ff6c54c3404eccf3f
dlqudtjs <dlqudtjs730@naver.com>
2022-09-10 17:11:59 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,6 +18,8 @@ const id = document.querySelector("#id"),
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(req),
-        });
+        })
+        .then((res) => res.json())
+        .then((res) => {});
     }
     
