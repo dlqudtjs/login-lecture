598c2e64d145042d2bbe762136da8b5bfa0f2e2b
dlqudtjs <dlqudtjs730@naver.com>
2022-09-17 17:55:51 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,40 +1,50 @@
 "use strict"
 
+const fs = require("fs").promises;
+
 class UserStorage {
-	static #users = {
-		id: ["a", "b", "c"],
-		psword: ["1", "1", "2"],
-		name: ["ab", "bb", "bc"],
-	};
-
-	static getUsers(...fields) {
-		const users = this.#users;
-		const newUsers = fields.reduce((newUsers, field) => {
-			if (users.hasOwnProperty(field)) {
-				newUsers[field] = users[field];
-			}
-			return newUsers;
-		}, {}); 		
-		return newUsers;
-	}
-
-	static getUserInfo(id) {
-		const users = this.#users;
+	static #getUserInfo(data, id) {
+		const users = (JSON.parse(data));
 		const idx = users.id.indexOf(id);
 		const userKeys = Object.keys(users); // => [id, psword, name]
 		const userInfo = userKeys.reduce((newUser, info) => {
 			newUser[info] = users[info][idx];
+
 			return newUser;
 		}, {});
 
 		return userInfo;
 	}
 
+	static getUsers(...fields) {
+		// const users = this.#users;
+		const newUsers = fields.reduce((newUsers, field) => {
+			if (users.hasOwnProperty(field)) {
+				newUsers[field] = users[field];
+			}
+		
+			return newUsers;
+		}, {}); 	
+
+		return newUsers;
+	}
+
+	static getUserInfo(id) {
+		return fs
+			.readFile("./src/databases/users.json")
+			.then((data) => {
+
+				return this.#getUserInfo(data, id);
+			})
+			.catch(console.err);
+	}
+
 	static save(userInfo) {
-		const users = this.#users;
+		// const users = this.#users;
 		users.id.push(userInfo.id);
 		users.name.push(userInfo.name);
 		users.psword.push(userInfo.psword);
+		
 		return { success: true };
 	}
 };
